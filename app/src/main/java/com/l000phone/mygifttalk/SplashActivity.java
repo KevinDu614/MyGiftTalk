debcdb634bdd52172a8a679af090aeb28c290728
Leon <Lv912844724@163.com>
2016-07-06 09:53:39 +0800

    吕斌添加了util包(xutils,picasso)

--- app/src/main/java/com/l000phone/mygifttalk/SplashActivity.java
+++ app/src/main/java/com/l000phone/mygifttalk/SplashActivity.java
@@ -3,12 +3,15 @@ package com.l000phone.mygifttalk;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.lidroid.xutils.ViewUtils;
+
 public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-        IllegalStateException  illegalStateException;
+        IllegalStateException illegalStateException;
+        ViewUtils.inject(this);
     }
 }
